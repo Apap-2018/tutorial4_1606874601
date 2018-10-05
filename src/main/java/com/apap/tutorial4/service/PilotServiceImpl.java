05f212dd6ac47212d047ed90f0ab04b339ee6664
mona.natasha <mona_natasha@ymail.com>
2018-10-05 10:20:47 +0700

    first commit latihan nomor 1

---
+++ src/main/java/com/apap/tutorial4/service/PilotServiceImpl.java
@@ -0,0 +1,24 @@
+package com.apap.tutorial4.service;
+import com.apap.tutorial4.model.PilotModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.apap.tutorial4.repository.PilotDb;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PilotServiceImpl implements PilotService {
+	@Autowired
+	private PilotDb pilotDb;
+	
+	@Override
+	public PilotModel getPilotDetailByLicenseNumber(String licenseNumber) {
+		return pilotDb.findByLicenseNumber(licenseNumber);
+	}
+	
+	@Override
+	public void addPilot(PilotModel pilot) {
+		pilotDb.save(pilot);
+	}
+
+}
